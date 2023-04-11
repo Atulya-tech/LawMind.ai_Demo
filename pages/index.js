25d9a1bcb79b59a4df9d5394f4c2edfa0772c50f
Alex Shibu <44331371+alexshibu1@users.noreply.github.com>
2023-04-11 14:27:32 -0400

    newer

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
 import { useState } from 'react';
 
 const Home = () => {
   const [userInput, setUserInput] = useState('');
   const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
 
-  const [isGenerating, setIsGenerating] = useState(false)
-  const [selectedFile, setSelectedFile] = useState(null);  // Add this line
   const onFileSelected = (event) => {
     setSelectedFile(event.target.files[0]);
   };
@@ -28,7 +25,7 @@ const Home = () => {
       });
     }
   
-    console.log("Calling OpenAI...")
+    console.log("Calling OpenAI...");
     const response = await fetch('/api/generate', {
       method: 'POST',
       headers: {
@@ -39,15 +36,15 @@ const Home = () => {
   
     const data = await response.json();
     const { output } = data;
-    console.log("OpenAI replied...", output.text)
+    console.log("OpenAI replied...", output.text);
   
     setApiOutput(`${output.text}`);
     setIsGenerating(false);
   };
-    const onUserChangedText = (event) => {
+
+  const onUserChangedText = (event) => {
     setUserInput(event.target.value);
   };
-  
 
   return (
     <div className="root">
@@ -57,50 +54,42 @@ const Home = () => {
             <h1>Need Feedback?</h1>
           </div>
           <div className="header-subtitle">
-            <h2>input the link or paste the content you would like feedback on</h2>
+            <h2>Input the link or paste the content you would like feedback on</h2>
           </div>
         </div>
-        {<div className="prompt-container">
-        <textarea
-placeholder="paste content or link to content(Google docs doesn't work :("
-className="prompt-box"
-  value={userInput}
-  onChange={onUserChangedText}                                   
-/>
-    <div className="prompt-buttons">
-  <a
-    className={isGenerating ? 'generate-button loading' : 'generate-button'}
-    onClick={callGenerateEndpoint}
-  >
-    <div className="generate">
-    {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
-    </div>
-  </a>
-</div>
-
-  {}
-  {apiOutput && (
-  <div className="output">
-    <div className="output-header-container">
-      <div className="output-header">
-        <h3>Output</h3>
-      </div>
-    </div>
-    <div className="output-content">
-      <p>{apiOutput}</p>
-    </div>
-  </div>
-)}
-</div>
-}
-        
-
-        
-        </a>
+        <div className="prompt-container">
+          <textarea
+            placeholder="Paste content or link to content (Google Docs doesn't work :("
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 };
 
 export default Home;
-
